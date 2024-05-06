75e18a1340021637c6d7e05327277885cafaa463
nobregas <gabrielnobregasantos2@gmail.com>
2024-05-06 13:31:03 -0300

    feat: initial commit

---
+++ src/main/java/br/com/lanchonete/lanchoneteapi/LanchoneteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lanchonete.lanchoneteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanchoneteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lanchonete/lanchoneteapi/LanchoneteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lanchonete.lanchoneteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
